1fa78c0c0f6ad7dcb43a9f531ce4e7b2319ee268
vikashk-tech <vikashk.dream@gmail.com>
2025-01-12 20:06:08 +0530

    mul updated

--- JAVA8/src/number/MulNo.java
+++ JAVA8/src/number/MulNo.java
@@ -13,7 +13,8 @@ public class MulNo {
 	public static void main(String[] args) {
 		
 		Integer[] intArray= {1,2,3,4,5};//120
-		List<Integer> list=Arrays.asList(1,2,3,4,5);
+		//List<Integer> list=Arrays.asList(1,2,3,4,5);
+		List<Integer> list=Arrays.asList(intArray);
 		Optional<Integer> outPut=list.stream().reduce((a,b)->a*b);
 		if(outPut.isPresent()) {
 			//System.out.println("Multplying no"+outPut);
@@ -21,6 +22,13 @@ public class MulNo {
 		}else {
 			System.out.println("Empty");
 		}
+		
+		//2nd method
+		Integer out=1;
+		for(Integer no:list) {		
+			out *=no;
+		}
+		System.out.println("Mul no: "+out);
 
 	}
 
